c93f98ddb75cfd2650d6d7f4854e0bab5720bde8
Jcgalvis6 <jcgalvis6@hotmail.com>
2018-06-06 17:24:50 -0500

    Se realiza el enlaze del proyecto de clases con el index

--- ClassFilosofos/ClassTenedores.cs
+++ ClassFilosofos/ClassTenedores.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ClassFilosofos
 {
@@ -13,7 +14,31 @@ namespace ClassFilosofos
 
         public void obtenerTenedores(int tenedorIzquierda, int tenedorDerecha)
         {
+            //El siguiente bloque de instrucciones, ser marca como una sección crítica (acceso a recurso compartido), 
+            //	utilizando el bloqueo de exclusión mutua (sentencia 'lock'). El bloque se libera cuando finaliza su ejecución.
+            //Si otro subproceso intenta acceder, esperará hasta que el objeto se libere (sincronización)
+            lock (this)
+            {
+                //Espera a que los 2 tenedores estén libres (ambos igual a false)
+                while (cantidadDeTenedores[tenedorIzquierda] || cantidadDeTenedores[tenedorDerecha])
+                    Monitor.Wait(this);
 
+                //Asigna true para especificar que el recurso está ocupado
+                cantidadDeTenedores[tenedorIzquierda] = true;
+                cantidadDeTenedores[tenedorDerecha] = true;
+            }
+        }
+
+        public void liberarTenedor(int tenedorIzquierda, int tenedorDerecha)
+        {
+            lock (this)
+            {
+                //Asigna false para especificar que el recurso está liberado
+                cantidadDeTenedores[tenedorIzquierda] = false;
+                cantidadDeTenedores[tenedorDerecha] = false;
+                //Avisa a los demás procesos el cambio de estatus del recurso compartido
+                Monitor.PulseAll(this);
+            }
         }
     }
 }
