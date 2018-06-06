ccb5a8de81ce2326164aededf8a42950961b9fab
Jcgalvis6 <jcgalvis6@hotmail.com>
2018-06-06 17:15:01 -0500

    Inicial

---
+++ ClassFilosofos/ClassTenedores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFilosofos
+{
+    public class ClassTenedores
+    {
+        bool[] cantidadDeTenedores = new bool[int.Parse(ConfigurationManager.AppSettings["NumeroDeFilosofos"])];
+
+        public void obtenerTenedores(int tenedorIzquierda, int tenedorDerecha)
+        {
+
+        }
+    }
+}
